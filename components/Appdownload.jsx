c49aa88f247b5d3ca5e766fa230e3eb9b2944c91
Benjamin Baraza <barazabenjamin58@gmail.com>
2026-03-19 12:05:02 +0300

    changes

--- components/Appdownload.jsx
+++ components/Appdownload.jsx
@@ -16,15 +16,17 @@ const appdownload = () => {
         <p className="text-center mt-4 text-gray-600 text-md">
           End-to-end payments and financial management in a single solution.
         </p>
-        <div className="flex justify-center mt-6 gap-4">
-          <button className="bg-[#5D5FEF] text-white px-4 py-2 rounded-md ml-4 mt-5 mb-5 hover:bg-[#4a4edb] transition duration-300 flex items-center gap-2">
-            <FaApple />
-            Play Store
-          </button>
-          <button className="bg-[#5D5FEF] text-white px-4 py-2 rounded-md ml-4 mt-5 mb-5 hover:bg-[#4a4edb] transition duration-300 flex items-center gap-2">
-            <FaGooglePlay />
-            App Store
-          </button>
+        <div>
+          <div className="flex justify-center mt-6 gap-4">
+            <button className="bg-[#5D5FEF] text-white px-4 py-2 rounded-md ml-4 mt-5 mb-5 hover:bg-[#4a4edb] transition duration-300 flex items-center gap-2">
+              <FaApple />
+              Play Store
+            </button>
+            <button className="bg-[#5D5FEF] text-white px-4 py-2 rounded-md ml-4 mt-5 mb-5 hover:bg-[#4a4edb] transition duration-300 flex items-center gap-2">
+              <FaGooglePlay />
+              App Store
+            </button>
+          </div>
         </div>
       </div>
     </section>
