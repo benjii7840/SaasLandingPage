05aa38bac29b83c59ba2db1843d28e19e7986bfc
Benjamin Baraza <barazabenjamin58@gmail.com>
2026-03-19 12:15:54 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import Navbar from "../components/Navbar";
-import Homepage from "../components/Homepage";
-import Sponsors from "../components/Sponsors";
-import Whatwedo from "../components/Whatwedo";
-import CTA from "../components/CTA";
-import WhyUs from "../components/WhyUs";
-import Appdownload from "../components/Appdownload";
-import Blogpost from "../components/Blogpost";
-import Connect from "../components/Connect";
-import Footer from "../components/Footer";
+import Navbar from "./components/Navbar";
+import Homepage from "./components/Homepage";
+import Sponsors from "./components/Sponsors";
+import Whatwedo from "./components/Whatwedo";
+import CTA from "./components/CTA";
+import WhyUs from "./components/WhyUs";
+import Appdownload from "./components/Appdownload";
+import Blogpost from "./components/Blogpost";
+import Connect from "./components/Connect";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
